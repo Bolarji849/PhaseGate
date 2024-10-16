eb138bc9cf8ad1fd2a0eeabd2514a0c08c207a10
Bolarji849 <mbolaji209@gmail.com>
2024-10-16 22:17:03 +0100

    SwapNumbers.java

---
+++ SwapNumbers.java
@@ -0,0 +1,20 @@
+public class SwapNumbers{
+
+
+	public int [] getSwapNumber(int [] array){
+	
+		for(int count = 1; count < array.length; count++){
+
+			if(count % 2 != 0){
+    			int temp = array[count];
+    			array[count] = array[count-1]; 
+  			array[count-1] = temp;
+		
+		}
+
+
+
+}return array;
+}
+}
+
